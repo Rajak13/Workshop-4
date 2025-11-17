6a32e8a86a797f063211179be1d03d82d1b87150
Rajak13 <queresma05@gmail.com>
2025-11-17 17:39:46 +0545

    Learned the concept of debugging through breakpoints

--- Task-6/Program.cs
+++ Task-6/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace Task_6;
+﻿using System;
 
-class Program
+namespace Task_6
 {
-    static void Main(string[] args)
+    public class Program
     {
-        Console.WriteLine("Hello, World!");
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter marks: ");
+            int.TryParse(Console.ReadLine(), out int marks);
+
+            Console.Write("Enter total: ");
+            int.TryParse(Console.ReadLine(), out int total);
+
+            double percentage = (double)marks / total * 100;
+
+            Console.WriteLine($"Percentage: {percentage}");
+        }
     }
 }
